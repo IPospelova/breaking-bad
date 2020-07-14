e1860f2af31a25584e379c7fd06a732198e68737
Irine <pospeliri@gmail.com>
2020-07-14 18:40:05 +0300

    add component, add v-for

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <top-nav></top-nav>
+    <div class="row">
+      <div class="col-md-3">
+        <sidebar></sidebar>
+      </div>
+      <div class="col-md-9">
+        <gallery></gallery>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Topnav from '@/components/block/Topnav.vue'
+import Sidebar from '@/components/block/Sidebar.vue'
+import Gallery from '@/components/block/Gallery.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'app',
+  components: {
+    'top-nav': Topnav,
+    sidebar: Sidebar,
+    gallery: Gallery
   }
 }
-</style>
+</script>
---
+++ src/components/block/Gallery.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="gallery">
+    <div class="row">
+      <div v-for="character in characters" :key="character.id" class="col-md-3">
+        <!-- gallery__card -->
+        <div class="gallery__card">
+          <div class="gallery__img-wrap">
+            <img :src="character.imgSrc" alt="" class="gallery__img" />
+          </div>
+          <div class="gallery__bottom">
+            <h3 class="gallery__nick">nike "{{ character.nike }}"</h3>
+            <p class="gallery__name">name "{{ character.name }}"</p>
+            <p class="gallery__date">birthday: {{ character.birthday }}</p>
+          </div>
+        </div>
+
+        <!-- gallery__card END -->
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'gallery',
+  data() {
+    return {
+      characters: [
+        {
+          imgSrc:
+            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+          nike: 'Heisenberg1',
+          name: 'Walter White1',
+          birthday: '09-07-1955'
+        },
+        {
+          imgSrc:
+            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+          nike: 'Heisenberg2',
+          name: 'Walter White2',
+          birthday: '09-07-1956'
+        },
+        {
+          imgSrc:
+            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+          nike: 'Heisenberg3',
+          name: 'Walter White3',
+          birthday: '09-07-1957'
+        },
+        {
+          imgSrc:
+            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+          nike: 'Heisenberg4',
+          name: 'Walter White4',
+          birthday: '09-07-1958'
+        },
+        {
+          imgSrc:
+            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+          nike: 'Heisenberg5',
+          name: 'Walter White5',
+          birthday: '09-07-1959'
+        }
+      ]
+    }
+  }
+}
+</script>
