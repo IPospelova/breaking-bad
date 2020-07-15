0430b35a57e9975765732a86e3faee534247105b
Irine <pospeliri@gmail.com>
2020-07-15 18:02:36 +0300

    add route, make Characters.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-    <top-nav></top-nav>
-    <div class="row">
-      <div class="col-md-3">
-        <sidebar></sidebar>
-      </div>
-      <div class="col-md-9">
-        <gallery></gallery>
-      </div>
-    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Topnav from '@/components/block/Topnav.vue'
-import Sidebar from '@/components/block/Sidebar.vue'
-import Gallery from '@/components/block/Gallery.vue'
+// import Topnav from '@/components/block/Topnav.vue'
+// import Sidebar from '@/components/block/Sidebar.vue'
+// import Gallery from '@/components/block/Gallery.vue'
 
 export default {
-  name: 'app',
-  components: {
-    'top-nav': Topnav,
-    sidebar: Sidebar,
-    gallery: Gallery
-  }
+  name: 'app'
 }
 </script>
---
+++ src/components/pages/Characters.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <top-nav></top-nav>
+    <div class="row">
+      <div class="col-md-3">
+        <sidebar></sidebar>
+      </div>
+      <div class="col-md-9">
+        <gallery></gallery>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Topnav from '@/components/block/Topnav.vue'
+import Sidebar from '@/components/block/Sidebar.vue'
+import Gallery from '@/components/block/Gallery.vue'
+
+export default {
+  name: 'Characters',
+  components: {
+    'top-nav': Topnav,
+    sidebar: Sidebar,
+    gallery: Gallery
+  }
+}
+</script>
