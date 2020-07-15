c64aa54c98366497631d5e61c696c548eb48df0c
Irine <pospeliri@gmail.com>
2020-07-15 18:45:55 +0300

    add <route-link> in header

--- src/components/block/Topnav.vue
+++ src/components/block/Topnav.vue
@@ -2,11 +2,20 @@
   <header class="header">
     <div class="container">
       <nav class="nav nav--navbar">
-        <a href="/" class="link link--navbar">characters</a>
+        <router-link to="/" class="link link--navbar">characters</router-link>
+        <router-link to="/seasons" class="link link--navbar"
+          >seasons</router-link
+        >
+        <router-link to="/episodes" class="link link--navbar"
+          >episodes</router-link
+        >
+        <router-link to="/quote" class="link link--navbar">quote</router-link>
+        <router-link to="/deaths" class="link link--navbar">deaths</router-link>
+        <!-- <a href="/" class="link link--navbar">characters</a>
         <a href="/seasons" class="link link--navbar">seasons</a>
         <a href="" class="link link--navbar">episodes</a>
         <a href="" class="link link--navbar">quote</a>
-        <a href="" class="link link--navbar">deaths</a>
+        <a href="" class="link link--navbar">deaths</a> -->
       </nav>
     </div>
   </header>
