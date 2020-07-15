526ae83f06062ecf44dc922b596d5221f1b172a9
Irine <pospeliri@gmail.com>
2020-07-15 18:30:38 +0300

    add link in header(Topnav)

--- src/components/block/Topnav.vue
+++ src/components/block/Topnav.vue
@@ -2,8 +2,8 @@
   <header class="header">
     <div class="container">
       <nav class="nav nav--navbar">
-        <a href="" class="link link--navbar">characters</a>
-        <a href="" class="link link--navbar">seasons</a>
+        <a href="/" class="link link--navbar">characters</a>
+        <a href="/seasons" class="link link--navbar">seasons</a>
         <a href="" class="link link--navbar">episodes</a>
         <a href="" class="link link--navbar">quote</a>
         <a href="" class="link link--navbar">deaths</a>
